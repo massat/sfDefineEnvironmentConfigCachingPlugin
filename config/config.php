f646aa154d6ac8de7e39c3c12db706a11f872a28
massat <mail@massat.jp>
2010-02-05 19:29:55 +0900

    * README * add comment

--- config/config.php
+++ config/config.php
@@ -1,8 +1,12 @@
 <?php
 
+// load configurations
 include $this->configCache->checkConfig('config/config_handlers.yml');
 
 foreach($this->handlers as $name => $configHandler) {
+    
+    // only configurations handled by sfDefineEnvironmentConfigHandler
+    // and set cache enabled will be cached.
     if(
         $configHandler instanceof sfDefineEnvironmentConfigHandler
         &&
