fb86c01e1ac4b7d9f83375e28766ff9b51f5da9a
massat <mail@massat.jp>
2010-02-10 09:00:35 +0900

    set caching default

--- config/config.php
+++ config/config.php
@@ -5,12 +5,11 @@ include $this->configCache->checkConfig('config/config_handlers.yml');
 
 foreach($this->handlers as $name => $configHandler) {
     
-    // only configurations handled by sfDefineEnvironmentConfigHandler
-    // and set cache enabled will be cached.
+    // only configurations handled by sfDefineEnvironmentConfigHandler will be cached.
     if(
         $configHandler instanceof sfDefineEnvironmentConfigHandler
         &&
-        $configHandler->getParameterHolder()->get('cache', false)
+        $configHandler->getParameterHolder()->get('cache', true)
     ) {
         $this->configCache->import($name, false);
     }
